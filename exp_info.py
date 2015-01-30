da32fda20fa63e8f7fc710ecbd3237fe3390d5f3
ptaeck <ptaeck@gmail.com>
2015-01-30 15:59:33 +0100

    Update and rename experiment_info.py to exp_info.py
    Template file is now generated by script print_template.py

---
+++ exp_info.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python    
+#exp_info.py - editable template for ploTRA experiment analysis
+#This file was generated on 2014-12-30 14:55:45
+
+import collections
+d0 = collections.OrderedDict()
+d1 = collections.OrderedDict()
+
+#===================
+# 0 GROUP EXP INFO 
+#===================
+
+d1['0_1.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['0_2.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['0_3.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['0_4.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+
+d0['0'] = d1
+d1 = collections.OrderedDict()
+
+#===================
+# 45 GROUP EXP INFO 
+#===================
+
+d1['45_5.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['45_6.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['45_7.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+
+d0['45'] = d1
+d1 = collections.OrderedDict()
+
+#===================
+# 90 GROUP EXP INFO 
+#===================
+
+d1['90_5.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['90_6.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+d1['90_7.TRA'] = {
+                 'grip_length' :0.1,
+                 'material' :'skelna tkanina',
+                 'weigth' :0.015,
+                 'cykly_horni_uvrat' :0.001,
+                 'cykly_spodni_uvrat' :100.0,
+                 'date_of_measuring' :'1.1.2015',
+                 'temperature' :21,
+                 'notes' :'Tkanina 3x Aeroglass 390g/m2, 0/90, matrice Epicote HGS LR285 + LH286',
+                 'thickness' :0.0011,
+                 'width' :0.015,
+                 'ext_length' :0.06,
+                 'name_of_tester' :'Frantisek Tester',
+                 'total_length' :0.135,
+                 }
+
+d0['90'] = d1
+d1 = collections.OrderedDict()
